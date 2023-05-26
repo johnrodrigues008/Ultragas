89f7a486d29f953244e612686c3642f30691552b
Lucca Marques <lucca.teles@rocky.ag>
2023-05-26 19:00:08 -0300

    Mais ligações entre os campos

--- Teste A - Formulário Único/Teste-A.js
+++ Teste A - Formulário Único/Teste-A.js
@@ -4,7 +4,6 @@ function formsA() {
 
   
   var styles = `
-
     .optimize .et_pb_column_10 {
       border-width: 0px;
     }
@@ -22,27 +21,68 @@ function formsA() {
   `;
     
   addStyle(styles);
+  connectForms();
 
   // funcao de ligacao entre os forms
   function connectForms (){
-    // elementos do novo forms
+    // elementos do NOVO FORMS
     let newForm = document.querySelector("form.rck-box-form");
+
+    // dados pessoais
     let inputNome = newForm.querySelector("#wpforms-651-field_1");
     let inputSobrenome = newForm.querySelector("#wpforms-651-field_2");
     let inputEmail = newForm.querySelector("#wpforms-651-field_3");
     let inputTelefone = newForm.querySelector("#wpforms-651-field_35");
 
+    // casa ou empresa
     let inputCasa = newForm.querySelector("#wpforms-651-field_11_1");
     let inputEmpresa = newForm.querySelector("#wpforms-651-field_11_2");
 
+    // local e custo
     let selectEstado = newForm.querySelector("#wpforms-651-field_34");
     let selectDistribuidora = newForm.querySelector("#wpforms-651-field_6");
     let selectCusto = newForm.querySelector("#wpforms-651-field_16");
 
+    // acordos
     let inputTermosDeUso = newForm.querySelector("#wpforms-651-field_18_1");
     let inputReceberConteudo = newForm.querySelector("#wpforms-651-field_29_1");
 
+    // BOTAO ENVIAR
+    let btnSubmit = newForm.querySelector('');
 
+    // FORMS ORIGINAL
+    let originalForm = document.querySelector('form#wpforms-form-651');
+    
+    // armazenando todos os inputs e selects em uma lista
+    var inputs = [inputNome, inputSobrenome, inputEmail, inputTelefone, inputCasa, 
+                  inputEmpresa, inputTermosDeUso, inputReceberConteudo];
+    
+    var selects = [selectEstado, selectDistribuidora, selectCusto];
+    
+    // vinculando campos
+    vincularInputs(inputs, originalForm);
+    vincularSelects(selects, originalForm);
+
+    function vincularInputs(inputs, form) {
+      inputs.forEach(function(input) {
+        input.addEventListener('input', function() {
+          form[input.id].value = input.value;
+        });
+      });
+    }
+
+    function vincularSelects(selects, form) {
+      selects.forEach(function(select) {
+        select.addEventListener('change', function() {
+          form[select.id].value = select.value;
+        });
+      });
+    }    
+
+    // vinculando submit
+    btnSubmit.addEventListener('click', function(){
+      originalForm.submit();
+    });
 
   }
 
