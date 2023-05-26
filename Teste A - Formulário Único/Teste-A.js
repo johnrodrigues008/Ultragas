e82b264cb1172e43295b975e167ff4332411624a
Lucca Marques <lucca.teles@rocky.ag>
2023-05-26 15:04:31 -0300

    Ligação entre os forms iniciada

--- Teste A - Formulário Único/Teste-A.js
+++ Teste A - Formulário Único/Teste-A.js
@@ -1,37 +1,69 @@
-function CardMob() {
+// ULTRA - Formulqrio de cadastro na primeira dobra, versao A
 
-      function addStyle(styles) {
-            var css = document.createElement("style");
-            css.type = "text/css";
+function formsA() {
 
-            if (css.styleSheet) css.styleSheet.cssText = styles;
-            else css.appendChild(document.createTextNode(styles));
+  
+  var styles = `
 
-            document.getElementsByTagName("head")[0].appendChild(css);
+    .optimize .et_pb_column_10 {
+      border-width: 0px;
+    }
+
+    .optimize .et_pb_column.et_pb_column_11 {
+      display:none;
+    }
+
+    @media (min-width: 981px){
+      .optimize .et_pb_column.et_pb_column_10 {
+        margin: 0 25%;
+        padding: unset;
       }
-      var styles = `
-            .wpforms-page.wpforms-page-1 {
-                  display: none!important;
-            }
+    }
+  `;
+    
+  addStyle(styles);
 
-            .wpforms-page.wpforms-page-2 {
-                  display: none!important;
-            }
+  // funcao de ligacao entre os forms
+  function connectForms (){
+    // elementos do novo forms
+    let newForm = document.querySelector("form.rck-box-form");
+    let inputNome = newForm.querySelector("#wpforms-651-field_1");
+    let inputSobrenome = newForm.querySelector("#wpforms-651-field_2");
+    let inputEmail = newForm.querySelector("#wpforms-651-field_3");
+    let inputTelefone = newForm.querySelector("#wpforms-651-field_35");
 
-            .wpforms-page.wpforms-page-3 {
-                  display: none!important;
-            }
+    let inputCasa = newForm.querySelector("#wpforms-651-field_11_1");
+    let inputEmpresa = newForm.querySelector("#wpforms-651-field_11_2");
 
-            .wpforms-page.wpforms-page-4.last {
-                  display: none!important;
-            }
+    let selectEstado = newForm.querySelector("#wpforms-651-field_34");
+    let selectDistribuidora = newForm.querySelector("#wpforms-651-field_6");
+    let selectCusto = newForm.querySelector("#wpforms-651-field_16");
 
-            .wpforms-submit-container {
-                  display: none!important;
-            }
-      `;
+    let inputTermosDeUso = newForm.querySelector("#wpforms-651-field_18_1");
+    let inputReceberConteudo = newForm.querySelector("#wpforms-651-field_29_1");
 
-      addStyle(styles);
+
+
+  }
+
+  function addStyle(styles){
+    document.body.classList.add("optimize");
+    
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.appendChild(document.createTextNode(styles));
+    
+    var head = document.getElementsByTagName("head")[0];
+    var styleSheets = head.getElementsByTagName("style");
+  
+    for (let i = 0; i < styleSheets.length; i++) {
+      let styleSheet = styleSheets[i];
+      if (styleSheet.innerHTML === css.innerHTML) {
+        return;
+      }
+    }
+    head.appendChild(css);
+  }
 }
 
-CardMob();
+  window.addEventListener("DOMContentLoaded", formsA());
